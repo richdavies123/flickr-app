c1de3c05fc759f55bf009e1faade4613e61dac27
Richard Davies <rdavies@doner.co.uk>
2016-03-30 09:34:13 +0100

    Added more details to details view

--- js/controllers.js
+++ js/controllers.js
@@ -2,14 +2,14 @@ var flickrControllers = angular.module('flickrControllers', []);
 
 flickrControllers.controller('ListPhotosController', ['$scope', '$http', function ListPhotosController($scope, $http){
     $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json').success(function(data) {
-    
+
     });
 
 	jsonFlickrFeed = function(data){
 		$scope.data = data;
 		$scope.photos = data.items;
 	}
-  
+
 }]);
 
 flickrControllers.controller('DetailPhotoController', ['$scope','$http', '$sce', '$routeParams', '$filter', function DetailPhotoController($scope, $http, $sce, $routeParams, $filter){
@@ -21,5 +21,9 @@ flickrControllers.controller('DetailPhotoController', ['$scope','$http', '$sce',
 		$scope.data = data;
 		$scope.photos = data.items;
 		$scope.whichItem = $routeParams.photoId;
+
+        $scope.toTrustedHTML = function( html ){
+            return $sce.trustAsHtml( html );
+        }
 	}
 }]);
